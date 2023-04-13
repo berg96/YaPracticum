639a55d96672ed64e67780ae5d024f7f19a1936f
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-13 19:00:44 +0200

    new task

--- return_func(less).py
+++ return_func(less).py
@@ -5,3 +5,9 @@ def calc_square(side_a, side_b):
 rectangle_area = calc_square(16, 9)
 
 print(rectangle_area)
+
+room1 = calc_square(3, 5)
+room2 = calc_square(4, 6)
+room3 = calc_square(3, 6)
+rooms_sum = room1 + room2 + room3
+print('Суммарная площадь комнат равна', rooms_sum, 'кв.м')
