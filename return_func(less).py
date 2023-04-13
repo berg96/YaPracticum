eff8c63a306fcd29b3ad6a184dd189424b4e36b8
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-13 19:07:00 +0200

    calc cube perimeter

--- return_func(less).py
+++ return_func(less).py
@@ -11,3 +11,8 @@ room2 = calc_square(4, 6)
 room3 = calc_square(3, 6)
 rooms_sum = room1 + room2 + room3
 print('Суммарная площадь комнат равна', rooms_sum, 'кв.м')
+
+def calc_cube_perimeter(side):
+    return side * 12
+
+print('Необходимый метраж палок для 8 кубов:', 8*calc_cube_perimeter(3))
