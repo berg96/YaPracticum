c21d50e731a51aed129988815f6d727899cf588e
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-14 23:18:04 +0200

    Напечатайте на экран: 1.Количество любимых песен Димы (только число). 2.Все любимые песни Сони, построчно.

--- dictinory(less).py
+++ dictinory(less).py
@@ -109,3 +109,13 @@ friends =  {
 
 for friend in friends:
       print(friend, 'живёт в городе', friends[friend])
+
+favorite_songs = {
+    'Серёга': ['Unforgiven', 'Holiday', 'Highway to hell'], 
+    'Соня': ['Shake it out', 'The Show Must Go On', 'Наше лето'], 
+    'Дима': ['Владимирский централ', 'Мурка', 'Третье сентября']
+}
+
+print(len(favorite_songs['Серёга']))
+for song in favorite_songs['Соня']:
+      print(song)
