80f7e2677bf63ea1fb36ef50b43a48747a08e9f8
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-14 23:11:05 +0200

    "Лена живёт в городе <город>"

--- dictinory(less).py
+++ dictinory(less).py
@@ -85,3 +85,11 @@ for singer in favorite_songs.values():
 for music in favorite_songs.keys():
 	print('Доктор, я каждый день по три раза слушаю песню ' + music)
         
+friends_names = ['Аня', 'Коля', 'Лёша', 'Лена', 'Миша']
+friends_cities = ['Владивосток', 'Красноярск', 'Москва', 'Обнинск', 'Чебоксары']
+friends =  {}
+
+for i in range(len(friends_names)):
+      friends[friends_names[i]] = friends_cities[i]
+
+print('Лена живёт в городе', friends['Лена'])
