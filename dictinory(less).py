b0b0fa9b2a918d485f7715f3a44c36bdc25a4746
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-14 23:28:50 +0200

    method add

--- dictinory(less).py
+++ dictinory(less).py
@@ -119,3 +119,24 @@ favorite_songs = {
 print(len(favorite_songs['Серёга']))
 for song in favorite_songs['Соня']:
       print(song)
+
+playlist = {
+    'Venus',
+    'Yesterday',
+    'Fireball',
+    'Time',
+    'Poison',
+    'Thunderstruck'
+}
+new_music = [
+    'Kashmir',
+    'Smoke on the Water',
+    'Bohemian Rhapsody',
+    'Zombie',
+    'Let It Be',
+    'Its My Life',
+]
+
+for song in new_music:
+      playlist.add(song)
+print(playlist)
