dbb732d249fd95db6ac5f3eda39e941aee788051
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-14 19:58:05 +0200

    Игра в города

--- set(less).py
+++ set(less).py
@@ -57,3 +57,23 @@ print(playlist_4)
 
 playlist_5=playlist_1.intersection(playlist_2)
 print(playlist_5)
+
+def print_valid_cities(all_cities,used_cities):
+    valid_cities=all_cities.difference(used_cities)
+    for city in valid_cities:
+        print(city)
+
+all_cities = {
+    'Абакан',
+    'Астрахань', 
+    'Бобруйск', 
+    'Калуга',
+    'Караганда',
+    'Кострома',
+    'Липецк', 
+    'Новосибирск'
+}
+
+used_cities = {'Калуга', 'Абакан' , 'Новосибирск'}
+
+print_valid_cities(all_cities, used_cities)
