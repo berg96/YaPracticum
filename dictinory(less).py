782ee512ecff5faa8dc08d75d840fa26ba81fa62
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-14 21:58:52 +0200

    add it in dic

--- dictinory(less).py
+++ dictinory(less).py
@@ -13,3 +13,13 @@ words_ru = list(english.keys())
 words_en = list(english.values())
 print(words_ru)
 print(words_en)
+print(set(english))
+
+favourites_songs_and_ratings = {}
+
+def add_favourite_song(song_name, rank):
+    favourites_songs_and_ratings[song_name] = rank
+    
+print('Первый вывод на печать: ', favourites_songs_and_ratings)
+add_favourite_song('Гражданская Оборона - Моя оборона', 5)
+print('Второй вывод на печать: ', favourites_songs_and_ratings)
