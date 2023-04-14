9664d50886c5bec2fa89c7ca7fa772196d1a2289
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-14 19:50:33 +0200

    method union

--- set(less).py
+++ set(less).py
@@ -47,3 +47,8 @@ playlist = {
 playlist.add('Thunderstruck')
 print(playlist)
 
+playlist_1 = {'Три белых коня', 'Happy new year', 'Снежинка'}
+playlist_2 = {'Last christmas', 'Снежинка', 'Happy new year'}
+playlist_3 = playlist_1.union(playlist_2)
+
+print(playlist_3)
