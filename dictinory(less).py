c8f3b8aa9a66fceaedb9096df5fa4710c82910a3
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-14 22:03:22 +0200

    new task

--- dictinory(less).py
+++ dictinory(less).py
@@ -19,7 +19,29 @@ favourites_songs_and_ratings = {}
 
 def add_favourite_song(song_name, rank):
     favourites_songs_and_ratings[song_name] = rank
-    
+
 print('Первый вывод на печать: ', favourites_songs_and_ratings)
 add_favourite_song('Гражданская Оборона - Моя оборона', 5)
 print('Второй вывод на печать: ', favourites_songs_and_ratings)
+
+friends =  {
+    'Серёга': 'Омск',
+    'Соня': 'Москва',
+    'Дима': 'Челябинск'
+}
+
+print(friends['Серёга'])
+friends['Серёга'] = 'Оренбург'
+print('Серёга теперь живёт в славном городе ' + friends['Серёга'])
+
+friends =  {
+    'Серёга': 'Оренбург',
+    'Соня': 'Москва',
+    'Миша': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь',
+    'Коля': 'Красноярск'
+}
+for city in set(friends.values()):
+    print(city)
