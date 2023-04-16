2db3114720841e81456d5eb1bfbddfd11c4ec090
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-16 14:47:25 +0200

    applying join

--- string(less).py
+++ string(less).py
@@ -69,3 +69,32 @@ for q in queries:
     result = check_query(q)
     print(q, '-', result)
 
+DATABASE = {
+    'Серёга': 'Омск',
+    'Соня': 'Москва',
+    'Миша': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь',
+    'Коля': 'Красноярск'
+}
+
+def process_anfisa(query):
+    if query == 'Сколько у меня друзей?':
+        count = len(DATABASE)
+        
+        return 'У тебя ' + str(count) + ' друзей.'
+    elif query == 'Кто все мои друзья?':
+        friends_string = ', '.join(DATABASE)
+        return 'Твои друзья: ' + friends_string
+    elif query == 'Где все мои друзья?':
+        cities_string = ', '.join(set(DATABASE.values()))
+        return 'Твои друзья в городах: ' + cities_string
+    else:
+        return '<неизвестный запрос>'
+
+print('Привет, я Анфиса!')
+print(process_anfisa('Сколько у меня друзей?'))
+print(process_anfisa('Кто все мои друзья?'))
+print(process_anfisa('Где все мои друзья?'))
+
