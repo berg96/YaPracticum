20ffa79404125d010f53fc1142d8a16b7f19ffa3
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-16 14:32:01 +0200

    new task

--- string(less).py
+++ string(less).py
@@ -51,3 +51,21 @@ print(penult_word(quote_1))
 print(penult_word(quote_2))
 print(penult_word(quote_3))
 
+def check_query(query):
+    elements  = query.split()
+    if elements[0] == 'Анфиса,':
+        return 'запрос к Анфисе'
+    else:
+        return 'запрос к кому-то ещё'
+
+queries = [
+    'Анфиса, сколько у меня друзей?',
+    'Андрей, ну где ты был?',
+    'Андрей, ну обними меня скорей!',
+    'Анфиса, кто все мои друзья?'
+]
+
+for q in queries:
+    result = check_query(q)
+    print(q, '-', result)
+
