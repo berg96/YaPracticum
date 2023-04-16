8826994be5f6f2af50bc8634546802d32d5b1d74
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-16 14:13:15 +0200

    I'm Python

--- string(less).py
+++ string(less).py
@@ -4,3 +4,10 @@ new_set = set(string)
 print('Список из строки: ' + str(new_list))
 print('Сет из строки: ' + str(new_set))
 
+monument_string = 'Я памятник себе воздвиг нерукотворный'
+
+index_list = [0, 1, 2, 8, 6, 17, 24]
+
+for i in index_list:
+    print(monument_string[i])
+
