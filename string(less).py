0fff917e17fc49d6f3230a1f0ce1f185f3a4a75a
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-16 15:19:40 +0200

    update

--- string(less).py
+++ string(less).py
@@ -111,3 +111,36 @@ def calc_stat(listened):
 
 print(calc_stat([189, 148, 210, 144, 174, 158, 163, 189, 227, 198]))
 
+def process_query(query):
+    name = query.split(', ')
+    if name[0] == 'Анфиса':
+        return process_anfisa(name[1])
+def format_friends_count(friends_count):
+    if friends_count == 1:
+        return '1 друг'
+    elif 2 <= friends_count <= 4:
+        return f'{friends_count} друга'
+    else:
+        return f'{friends_count} друзей'
+
+def process_anfisa(query):
+    if query == 'сколько у меня друзей?':
+        count = len(DATABASE)
+        return f'У тебя {format_friends_count(count)}.'
+    elif query == 'кто все мои друзья?':
+        friends_string = ', '.join(DATABASE)
+        return f'Твои друзья: {friends_string}'
+    elif query == 'где все мои друзья?':
+        unique_cities = set(DATABASE.values())
+        cities_string = ', '.join(unique_cities)
+        return f'Твои друзья в городах: {cities_string}'
+    else:
+        return '<неизвестный запрос>'
+
+print('Привет, я Анфиса!')
+print(process_query('Анфиса, сколько у меня друзей?'))
+print(process_query('Анфиса, кто все мои друзья?'))
+print(process_query('Анфиса, где все мои друзья?'))
+print(process_query('Анфиса, кто виноват?'))
+print(process_query('Соня, ты где?'))
+
