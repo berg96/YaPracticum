8783f2c9379664fc1ca8b1c832561fd59d1e552a
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-17 13:08:02 +0200

    date - date

--- library(less).py
+++ library(less).py
@@ -25,4 +25,6 @@ import datetime as dt
 start_time = dt.datetime(1961, 4, 12, 9, 7, 0)
 
 print('Уже', start_time, 'Поехали!') 
+landing_time = dt.datetime(1961, 4, 12, 10, 55, 0)
+print(landing_time - start_time) 
 
