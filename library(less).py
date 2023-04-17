c63bca92312a394276317207e155f7b4d39bba7a
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-17 13:32:34 +0200

    added the function of displaying the time in a specific city and a specific friend

--- library(less).py
+++ library(less).py
@@ -35,3 +35,41 @@ period = dt.timedelta(hours=3)
 moscow_time = utc_time + period
 print(moscow_time)
 
+import datetime as dt
+
+UTC_OFFSET = {
+    'Санкт-Петербург': 3,
+    'Москва': 3,
+    'Самара': 4,
+    'Новосибирск': 7,
+    'Екатеринбург': 5,
+    'Нижний Новгород': 3,
+    'Казань': 3,
+    'Челябинск': 5,
+    'Омск': 6,
+    'Ростов-на-Дону': 3,
+    'Уфа': 5,
+    'Красноярск': 7,
+    'Пермь': 5,
+    'Воронеж': 3,
+    'Волгоград': 3,
+    'Краснодар': 3,
+    'Калининград': 2
+}
+
+DATABASE = {
+    'Серёга': 'Омск',
+    'Соня': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь'
+}
+
+def what_time_city(city):
+    return dt.datetime.utcnow() + dt.timedelta(hours=UTC_OFFSET[city])
+
+def what_time_friend(friend):
+    return what_time_city(DATABASE[friend])
+
+print(what_time_city('Екатеринбург'))
+print(what_time_friend('Соня'))
