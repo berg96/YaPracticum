806ddd96b3cf85c22500bf29400e64394dfe54c0
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-17 17:05:20 +0200

    update

--- request(less).py
+++ request(less).py
@@ -3,4 +3,15 @@ import requests
 url = 'http://wttr.in/?0T'
 response = requests.get(url)
 print(response.status_code)
+#print(response.text)
 
+url = 'https://wttr.in'
+
+weather_parameters = {
+    '0': '',
+    'T': '1'
+}
+
+response = requests.get(url, params=weather_parameters)  # передайте параметры в http-запрос
+
+print(response.text)
