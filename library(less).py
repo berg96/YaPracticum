f3533a8cee679ae515a183bff48235b31127897a
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-17 12:58:08 +0200

    import "choice"

--- library(less).py
+++ library(less).py
@@ -3,3 +3,11 @@ import math
 square_root = math.sqrt(16)
 print(square_root)
 
+from random import choice
+
+def find_a_present(prizes):
+    return choice(prizes) 
+
+print(find_a_present(['кукла', 'жвачка', 'игрушечный питон']))
+print(find_a_present(['мяч', 'чебурашка', 'лосяш']))
+
