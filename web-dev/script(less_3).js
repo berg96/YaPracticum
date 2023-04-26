5f61b288e894e979352e0059a46c220d0c5bacde
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-26 21:54:52 +0200

    new exp

--- web-dev/script(less_3).js
+++ web-dev/script(less_3).js
@@ -2,4 +2,6 @@ console.log('Привет, мир!');
 console.log(40000 + 22000);
 let time;
 time = 34;
-console.log(time);
+console.log('Старт поездки. Осталось минут: ' + time);
+time -= 15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
