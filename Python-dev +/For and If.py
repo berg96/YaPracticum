5a861aa100ff8f26445a12ed1acbda577cfacf92
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-03 10:34:49 +0200

    exp For

---
+++ Python-dev +/For and If.py
@@ -0,0 +1,6 @@
+bremen_musicians = ['Кот', 'Пёс', 'Трубадур', 'Осёл', 'Петух']
+
+for musician in bremen_musicians:
+    print(musician)
+
+print('Нам дворцов заманчивые своды не заменят никогда свободы!')
