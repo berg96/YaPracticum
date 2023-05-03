ec3bfae1d0c40bc27c7e778f9ee4df32dabee28b
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-03 11:13:17 +0200

    nested loop

--- Python-dev +/For and If.py
+++ Python-dev +/For and If.py
@@ -4,3 +4,10 @@ for musician in bremen_musicians:
     print(musician)
 
 print('Нам дворцов заманчивые своды не заменят никогда свободы!')
+
+kids = ['Витя', 'Маша', 'Марина']
+candies = ['Батончик', 'Сникерс', 'Мишка Косолапый', 'Коровка']
+
+for kid in kids:
+    for candy in candies:
+        print(kid, 'получает конфету', candy)
