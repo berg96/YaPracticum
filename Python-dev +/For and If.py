2b3dda22e0bb1a506a910d677b03146b2cf811f0
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-03 11:50:05 +0200

    new exp

--- Python-dev +/For and If.py
+++ Python-dev +/For and If.py
@@ -18,3 +18,12 @@ for i in range(1, 5):
 seasons = ['зима', 'весна', 'лето', 'осень']
 for season in reversed(seasons):
     print(season) 
+
+countdown_str = ''
+
+for i in reversed(range(11)):
+    countdown_str += str(i) + ', '
+
+countdown_str = countdown_str + 'поехали!'
+
+print(countdown_str)
