468e6c5ae209595c29bcef7501b70f94740fbf10
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-04 23:09:16 +0200

    add param amount in func calc_cube

--- Python-dev +/Function.py
+++ Python-dev +/Function.py
@@ -66,13 +66,13 @@ def calc_cube_area(side):
     cube_area = one_face * 6
     return cube_area
 
-def calc_cube(side):
+def calc_cube(side,amount):
     one_cube_perimeter = calc_cube_perimeter(side)
-    full_length = one_cube_perimeter * 8
+    full_length = one_cube_perimeter * amount
 
     one_cube_area = calc_cube_area(side)
-    full_area = one_cube_area * 8
+    full_area = one_cube_area * amount
 
-    print('Для 8 кубов понадобится палок (м):', full_length, 'и стекла (кв.м):', full_area)
+    print('Для', amount, 'кубов понадобится палок (м):', full_length, 'и стекла (кв.м):', full_area)
 
-calc_cube(3)
+calc_cube(3,2)
