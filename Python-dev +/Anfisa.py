85d92006795c3ce475171ebd806fad5d11e21674
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-04 21:58:00 +0200

    add func say_hello

--- Python-dev +/Anfisa.py
+++ Python-dev +/Anfisa.py
@@ -9,3 +9,19 @@ print('Привет, я Анфиса!')
 friends = ['Сергей', 'Соня', 'Дима', 'Алина', 'Егор']
 
 print(friends)
+
+
+def say_hello(current_hour):
+    if current_hour <= 5 or current_hour >= 23:
+        print('Доброй ночи!')
+    elif current_hour >= 6 and current_hour <= 11:
+        print('Доброе утро!')
+    elif current_hour >= 12 and current_hour <= 17:
+        print('Добрый день!')
+    elif current_hour >= 18 and current_hour <= 22:
+        print('Добрый вечер!')
+
+say_hello(4)
+say_hello(10)
+say_hello(15)
+say_hello(20)
