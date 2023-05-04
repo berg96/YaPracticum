dd06fa2f25f475cdd475cd8fd1c27ef5a5c426af
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-04 21:53:20 +0200

    update

--- Python-dev +/Function.py
+++ Python-dev +/Function.py
@@ -1,13 +1,10 @@
-new_mail = 0
-
-
 def check_mail():
     if new_mail > 0:
         print("Пришло письмо, не пропусти!")
     else:
         print("Никто не пишет.")
 
-
+new_mail = 0
 check_mail()
 new_mail = 1
 check_mail()
@@ -16,13 +13,11 @@ check_mail()
 def hello(name):
     print(name + ", приветствую тебя!")
 
-
 hello("Максим")
 
 
 def hello(name, bonus):
     print(name + ", приветствую тебя! Бери " + bonus)
-
 
 hello("Дарт Вейдер", "печеньки")
 hello("Винни Пух", "мёд")
