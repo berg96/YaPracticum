1ce4d6f152b37af8ec2a45ee1935807d6582d339
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-04 21:44:03 +0200

    add func with 2 params

--- Python-dev +/Function.py
+++ Python-dev +/Function.py
@@ -1,16 +1,28 @@
 new_mail = 0
 
+
 def check_mail():
     if new_mail > 0:
-        print('Пришло письмо, не пропусти!')
+        print("Пришло письмо, не пропусти!")
     else:
-        print('Никто не пишет.')
+        print("Никто не пишет.")
+
 
 check_mail()
 new_mail = 1
 check_mail()
 
-def hello(name):
-    print(name + ', приветствую тебя!')
 
-hello('Максим')
+def hello(name):
+    print(name + ", приветствую тебя!")
+
+
+hello("Максим")
+
+
+def hello(name, bonus):
+    print(name + ", приветствую тебя! Бери " + bonus)
+
+
+hello("Дарт Вейдер", "печеньки")
+hello("Винни Пух", "мёд")
