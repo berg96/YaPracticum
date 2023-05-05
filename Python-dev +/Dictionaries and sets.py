770d7b0234bee83b6670fc863f9a455cf14670c6
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-05 21:44:08 +0200

    method difference (set1 - set2)

--- Python-dev +/Dictionaries and sets.py
+++ Python-dev +/Dictionaries and sets.py
@@ -60,3 +60,10 @@ playlist_2 = {'Last christmas', 'Снежинка', 'Happy new year'}
 playlist_3 = playlist_1.union(playlist_2)
 
 print(playlist_3)
+
+
+playlist_1 = {'Голубой вагон', 'Облака', 'Yesterday', 'Наше лето'}
+playlist_2 = {'Наше лето', 'Голубой вагон', 'Облака'}
+playlist_3 = playlist_1.difference(playlist_2)
+
+print(playlist_3)
