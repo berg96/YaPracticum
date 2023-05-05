851b602f7b44b809d177f1386b48760054556586
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-05 22:06:24 +0200

    add new elem in dict

--- Python-dev +/Dictionaries and sets.py
+++ Python-dev +/Dictionaries and sets.py
@@ -152,3 +152,17 @@ favorite_songs = {
 }
 
 print(favorite_songs.keys())
+
+
+favourites_songs_and_ratings = {}
+
+def add_favourite_song(song_name, rank):
+    favourites_songs_and_ratings[song_name] = rank
+
+# Словарь пока что пуст:
+print('Первый вывод на печать: ', favourites_songs_and_ratings)
+
+add_favourite_song('Гражданская Оборона - Моя оборона', 5)
+
+# А здесь словарь уже обновлен и не пуст:
+print('Второй вывод на печать: ', favourites_songs_and_ratings)
