b1150831de34a01b5b6b8f32b5391b77d5aab299
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-06 20:07:35 +0200

    update

--- Python-dev +/Anfisa.py
+++ Python-dev +/Anfisa.py
@@ -8,29 +8,37 @@ DATABASE = {
     'Коля': 'Красноярск'
 }
 
+# Новая функция, она возвращает правильное словосочетание, склоняя слово "друзья" 
+# в зависимости от того, какое число передано в аргументе friends_count
+def format_friends_count(friends_count):
+    if friends_count == 1:
+        return '1 друг'
+    elif 2 <= friends_count <= 4:
+        return f'{friends_count} друга'
+    else:
+        return f'{friends_count} друзей'
+
+
 def process_anfisa(query):
-    if query == 'Сколько у меня друзей?':
+    if query == 'сколько у меня друзей?':
         count = len(DATABASE)
-        return 'У тебя ' + str(count) + ' друзей.'
-    # Здесь проверьте, что переменная query равна строке 'Кто все мои друзья?'
-    elif query == 'Кто все мои друзья?':  
-        friends_string = 'Твои друзья: '
-        # Чтобы получить перечень друзей - 
-        # переберите словарь DATABASE в цикле
-        for friend in DATABASE:     
-            friends_string += friend + ' '     # Добавляйте к переменной friends_string имя друга и пробел
-        # Верните строку, составленную из 'Твои друзья: ' и friends_string 
-        return(friends_string)  
-    elif query == 'Где все мои друзья?':
-        cities_string = 'Твои друзья в городах: '
-        for city in set(DATABASE.values()):
-            cities_string += city + ' '
-        return cities_string
+        # Вызовите функцию format_friends_count() и передайте в неё count.
+        # Отредактируйте строку ниже: в ней должно использоваться выражение, 
+        # которое вернёт функция format_friends_count()
+        return f'У тебя {format_friends_count(count)}.'
+    elif query == 'кто все мои друзья?':
+        friends_string = ', '.join(DATABASE)
+        return f'Твои друзья: {friends_string}'
+    elif query == 'где все мои друзья?':
+        unique_cities = set(DATABASE.values())
+        cities_string = ', '.join(unique_cities)
+        return f'Твои друзья в городах: {cities_string}'
     else:
         return '<неизвестный запрос>'
 
-# Не изменяйте следующий код
+
 print('Привет, я Анфиса!')
-print(process_anfisa('Сколько у меня друзей?'))
-print(process_anfisa('Кто все мои друзья?'))
-print(process_anfisa('Где все мои друзья?'))
+print(process_anfisa('сколько у меня друзей?'))
+print(process_anfisa('кто все мои друзья?'))
+print(process_anfisa('где все мои друзья?'))
+print(process_anfisa('кто виноват?'))
