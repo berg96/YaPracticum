dd01a1524be823d25454c93002a976cc314987a5
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-06 19:32:49 +0200

    index of str

--- Python-dev +/Strings and formatting.py
+++ Python-dev +/Strings and formatting.py
@@ -3,3 +3,13 @@ new_list = list(string)
 new_set = set(string)
 print('Список из строки: ' + str(new_list))
 print('Сет из строки: ' + str(new_set))
+
+
+monument_string = 'Я памятник себе воздвиг нерукотворный'
+
+index_list = [0, 1, 2, 8, 6, 17, 24]
+
+for i in index_list:
+    # На каждой итерации цикла 
+    # берём из строки monument_string элемент с индексом i и печатаем полученную букву
+    print(monument_string[i])
