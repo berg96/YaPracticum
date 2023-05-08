71b149245ab2722c62bab14f5ad6c230a1ea2b43
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-08 23:02:20 +0200

    use lib urllib.parse methods quote() and unquote()

--- Python-dev +/HTTP Protocol.py
+++ Python-dev +/HTTP Protocol.py
@@ -3,3 +3,21 @@ user_query = 'как стать бэкенд-разработчиком'
 url = 'https://yandex.ru/search/?text=' +  '%20'.join(user_query.split())
 
 print(url)
+
+
+import urllib.parse
+
+strings = [
+    'Что такое backend',
+    'Привет!',
+    ' ',        # Просто пробел.
+    'letiště',  # Аэропорт по-чешски.
+    'München',  # Крупнейший город Баварии.
+    'Champs-Élysées',  # Елисейские поля.
+    '東京',     # А это Токио.
+]
+
+for s in strings:
+    encoded = urllib.parse.quote(s)          # Зашифрованная строка.
+    decoded = urllib.parse.unquote(encoded)  # Расшифрованная обратно строка.
+    print(decoded, '-', encoded)
