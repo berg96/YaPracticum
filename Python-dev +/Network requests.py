13daf3312f5d16233ed92f46a5a51d627c23215c
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-11 20:05:18 +0200

    found out about the weather

--- Python-dev +/Network requests.py
+++ Python-dev +/Network requests.py
@@ -5,3 +5,13 @@ import requests
 response = requests.get('http://info.cern.ch/')
 
 print(response.text)  # Печатаем код запрошенной страницы.
+
+
+import requests
+
+url = 'http://wttr.in/?0T'
+
+response = requests.get(url)  # Выполните HTTP-запрос.
+
+print(response.status_code)  # Напечатайте статус-код ответа.
+print(response.text)
