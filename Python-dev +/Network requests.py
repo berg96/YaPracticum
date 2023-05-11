417e80835ba3b714b884690613452b79218c3de4
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-11 20:35:42 +0200

    use try-except in requests

--- Python-dev +/Network requests.py
+++ Python-dev +/Network requests.py
@@ -66,3 +66,47 @@ request_headers = {
 # через аргументы `params` и `headers` функции get()
 response = requests.get(url, params = weather_parameters, headers = request_headers)
 print(response.text)
+
+
+import requests
+
+
+cities = [
+    'Омск',
+    'Калининград',
+    'Челябинск',
+    'Владивосток',
+    'Красноярск',
+    'Москва',
+    'Екатеринбург'
+]
+
+
+def make_url(city):
+    # в URL задаём город, в котором узнаем погоду
+    return f'http://wttr.in/{city}'
+
+
+def make_parameters():
+    params = {
+        'format': 2,  # погода одной строкой
+        'M': ''  # скорость ветра в "м/с"
+    }
+    return params
+
+
+def what_weather(city):
+    try:
+        response = requests.get(make_url(city),params=make_parameters())
+    except requests.ConnectionError:
+        return '<сетевая ошибка>'
+    if response.status_code == 200:
+        return response.text
+    else:
+        return '<ошибка на сервере погоды>'
+    # Напишите тело этой функции.
+    # Не изменяйте остальной код!
+
+print('Погода в городах:')
+for city in cities:
+    print(city, what_weather(city))
