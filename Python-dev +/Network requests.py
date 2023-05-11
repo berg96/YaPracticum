b4a78adfd8394d3a114a9ca2eae81f4f77fcfafb
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-11 20:14:13 +0200

    use .headers

--- Python-dev +/Network requests.py
+++ Python-dev +/Network requests.py
@@ -30,3 +30,18 @@ response = requests.get(url, params=search_parameters)
 
 print(response.status_code)
 print(response.url)
+
+
+import requests
+
+response = requests.get('https://ya.ru/white')
+
+# вот мы узнали, что код ответа 200 и заполучили это число в свой код:
+code = response.status_code
+print(f'Код ответа = {code}')
+
+# а вот мы и заголовки читаем, и выводим их форматированной строкой
+# с примечанием, каким захочется, на любом языке
+headers = response.headers
+print(f'Тип содержимого: {headers["content-type"]}')
+print(f'Время ответа: {headers["date"]}')
