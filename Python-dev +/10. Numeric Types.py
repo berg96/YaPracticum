97bdcd234ade667ee4568b98764fe31e50023be4
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-16 22:13:16 +0200

    1 task

---
+++ Python-dev +/10. Numeric Types.py
@@ -0,0 +1,11 @@
+day_1 = 1.434
+day_2 = 2.12
+day_3 = 1.632
+day_4 = 5.59
+day_5 = 2.542
+day_6 = 1.1
+day_7 = 1.324
+
+week_dist = (day_1*1000 + day_2*1000 +day_3*1000 + day_4*1000 + day_5*1000 + day_6*1000 + day_7*1000) / 1000
+
+print(week_dist)
