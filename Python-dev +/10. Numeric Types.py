42edab48aa9f7c658f78f3b47666e0edcc57ed27
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-16 22:13:29 +0200

    new exp

--- Python-dev +/10. Numeric Types.py
+++ Python-dev +/10. Numeric Types.py
@@ -9,3 +9,14 @@ day_7 = 1.324
 week_dist = (day_1*1000 + day_2*1000 +day_3*1000 + day_4*1000 + day_5*1000 + day_6*1000 + day_7*1000) / 1000
 
 print(week_dist)
+
+
+from decimal import Decimal
+
+i = 3.3 + 4.1
+print(i)
+# Вывод в терминал: 7.3999999999999995
+
+i = Decimal('3.3') + Decimal('4.1')
+print(i)
+# Вывод в терминал: 7.4
