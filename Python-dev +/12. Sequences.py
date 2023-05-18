c9a18d64f4dbbaf8ad5e29d75d09f8bb5ced438c
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-18 23:22:40 +0200

    new task

--- Python-dev +/12. Sequences.py
+++ Python-dev +/12. Sequences.py
@@ -6,3 +6,10 @@ print(name_movie[2])
 print(name_movie[10])
 print(name_movie[-2])
 print(name_movie[-1])
+
+
+a = 'Роботы стали важны'
+b = 'в период'
+c = 'эмиграции с Терры'
+
+print(a[4:7]+b[2]+b[4]+a[1]+c[3:6]+c[1]+c[1:3]+a[7:9])
