e70e0ec2f6cf6f5798ff6020b787beb18f1e520c
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-19 21:33:09 +0200

    change var = change id

--- Python-dev +/13. Variables and Memory.py
+++ Python-dev +/13. Variables and Memory.py
@@ -6,3 +6,12 @@ copy_brand_name = brand_name
 print(id(copy_brand_name))
 # Вывод в терминал: 25080896
 # ID совпадают, значит, обе переменные ссылаются на одну ячейку памяти.
+
+brand_name = 'Unicorn'
+print(id(brand_name))
+# Вывод в терминал: 25080896
+
+brand_name = 'Cat'
+print(id(brand_name))
+# Вывод в терминал: 27193914
+# ID разные: теперь brand_name ссылается на другую ячейку памяти.
