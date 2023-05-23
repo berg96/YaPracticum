741722d54f5138284ec47ac146aa501a014f813a
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-23 21:04:47 +0200

    new task

--- Python-dev +/19. Python Standard Library.py
+++ Python-dev +/19. Python Standard Library.py
@@ -28,3 +28,30 @@ maxim_birthday = dt.date(2011, 12, 16)
 
 print(lera_birthday)
 print(maxim_birthday)
+
+today = dt.date.today()
+
+print(today)
+
+import datetime as dt
+
+lera_birthday = dt.date(2015, 5, 16)
+maxim_birthday = dt.date(2011, 12, 16)
+
+today = dt.date.today()
+
+# Обратитесь к значению года
+# текущей даты через точечную нотацию
+today_year = today.year
+
+# Переопределите значение года на текущий
+# для дней рождений ребят
+lera_birthday = lera_birthday.replace(year=today_year)
+maxim_birthday = maxim_birthday.replace(year=today_year)
+
+# Получите разницу между днями рождения и текущей датой
+lera_days_left = lera_birthday - today
+maxim_days_left = maxim_birthday - today
+
+print(lera_days_left.days)
+print(maxim_days_left.days)
