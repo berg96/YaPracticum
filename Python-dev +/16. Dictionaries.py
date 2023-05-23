ab3657c84dcf48fb81a4b8c7c68643b709054977
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-23 19:43:45 +0200

    update

--- Python-dev +/16. Dictionaries.py
+++ Python-dev +/16. Dictionaries.py
@@ -308,13 +308,11 @@ recommended_movies = {
 # место для вашего кода
 for movie in recommended_movies:
     if recommended_movies[movie]['rating'] >= 4.0 :
-        review = recommended_movies[movie]['review']
-        print(f'У фильма "{movie}" хороший отзыв: "{review}". Фильм добавлен в избранное.')
+        print(f'У фильма "{movie}" хороший отзыв: "{recommended_movies[movie]["review"]}". Фильм добавлен в избранное.')
         favorite_movies[movie] = recommended_movies[movie]
     else:
         review = recommended_movies[movie]['review']
-        movies_for_delete.append(movie)
-        print(f'Фильм "{movie}" не интересен: "{review}". Фильм удалён из рекомендаций.')
+        print(f'Фильм "{movie}" не интересен: "{recommended_movies[movie]["review"]}". Фильм удалён из рекомендаций.')
 
 for movie in movies_for_delete:
     del recommended_movies[movie]
