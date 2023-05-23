eae9700cea1a81f567481c716c015c7da23282b0
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-23 22:01:08 +0200

    new task

--- Python-dev +/19. Python Standard Library.py
+++ Python-dev +/19. Python Standard Library.py
@@ -61,10 +61,14 @@ import datetime as dt
 def get_days_to_birthday(date_birthday):
     today = dt.date.today()
     today_year = today.year
+    date_birthday = date_birthday.replace(year=today_year)
     if date_birthday == today:
         return 0
-    date_birthday = date_birthday.replace(year=today_year)
-    return (date_birthday - today).days
+    elif date_birthday < today:
+        date_birthday = date_birthday.replace(year=today_year+1)
+        return (date_birthday - today).days
+    else:
+        return (date_birthday - today).days
 
 
 lera_birthday = dt.date(2015, 5, 16)
@@ -73,3 +77,34 @@ maxim_birthday = dt.date(2011, 12, 16)
 print(get_days_to_birthday(lera_birthday))
 print(get_days_to_birthday(maxim_birthday))
 print(get_days_to_birthday(dt.date.today()))
+
+import datetime as dt
+# В эту переменную запишите формат для
+# преобразования даты
+FORMAT = '%d.%m.%Y'
+
+# Добавьте в объявление функции ещё один параметр - имя
+def get_days_to_birthday(name, date_birthday):
+    # Преобразуйте полученную строку с датой в объект нужного типа
+    date_birthday = dt.datetime.strptime(date_birthday,FORMAT).date()
+		
+    today = dt.date.today()
+    date_birthday = date_birthday.replace(year=today.year)
+
+    if date_birthday < today:
+        date_birthday = date_birthday.replace(year=today.year + 1)
+
+    days_to_birthday = date_birthday - dt.date.today()
+    return f'{name}, до твоего дня рождения осталось дней: {days_to_birthday.days}'
+
+
+birthdays = [
+    ('Лера', '16.05.2015'),
+    ('Максим', '16.12.2011'),
+    ('Толя','12.06.2016')
+]
+
+# Напечатайте результат вызова функции get_days_to_birthday()
+# для каждой пары из списка birthdays 
+for name,birthday in birthdays:
+    print(get_days_to_birthday(name,birthday))
