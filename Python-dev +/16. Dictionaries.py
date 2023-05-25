cabf21f5b6216166db9e3dd8587cb99103946ed8
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-25 22:31:27 +0200

    new task

--- Python-dev +/16. Dictionaries.py
+++ Python-dev +/16. Dictionaries.py
@@ -318,3 +318,20 @@ for movie in movies_for_delete:
     del recommended_movies[movie]
 
 print(favorite_movies)
+
+data = {
+    'Singapore': 1,
+    'Ireland': 6,
+    'United Kingdom': 7,
+    'Germany': 27,
+    'Armenia': 34,
+    'United States': 17,
+    'Canada': 9,
+    'Italy': 74
+}
+
+country = 'Russia'
+if country in data:
+    print(data.get(country))
+else:
+    print('Not found')
