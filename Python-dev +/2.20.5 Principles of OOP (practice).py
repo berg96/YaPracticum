b9d2e277f2c36665753cdbcb14a55489c81603ef
Artem Kulikov <chigar2010@yandex.ru>
2023-06-12 14:06:30 +0200

    task 3

--- Python-dev +/2.20.5 Principles of OOP (practice).py
+++ Python-dev +/2.20.5 Principles of OOP (practice).py
@@ -3,7 +3,9 @@ class Bird:
         self.name: str = name
         self.size: str = size
 
-    def describe(self) -> str:
+    def describe(self, full: bool = False) -> str:
+        if full:
+            return 'Unexpected Error'
         return f'Размер птицы {self.name} — {self.size}.'
 
 
@@ -12,12 +14,37 @@ class Parrot(Bird):
         super().__init__(name, size)
         self.color: str = color
 
+    def describe(self, full: bool = False) -> str:
+        if full:
+            return (
+                f'Попугай {self.name} — заметная птица, '
+                f'окрас её перьев — {self.color}, а размер — {self.size}. '
+                'Интересный факт: попугаи чувствуют ритм, а вовсе не бездумно '
+                'двигаются под музыку. Если сменить композицию, '
+                'то и темп движений птицы изменится.'
+            )
+        return super().describe(full)
+
 
 class Penguin(Bird):
     def __init__(self, name: str, size: str, genus: str) -> None:
         super().__init__(name, size)
         self.genus: str = genus
 
+    def describe(self, full: bool = False) -> str:
+        if full:
+            return (
+                f'Размер пингвина {self.name} из '
+                f'рода {self.genus} — {self.size}. Интересный факт: '
+                'однажды группа геологов-разведчиков похитила пингвинье яйцо, '
+                'и их принялась преследовать вся стая, не пытаясь, '
+                'впрочем, при этом нападать. Посовещавшись, '
+                'похитители вернули птицам яйцо, и те отстали.'
+            )
+        return super().describe(full)
+
 
 kesha = Parrot('Ара', 'средний', 'красный')
 kowalski = Penguin('Королевский', 'большой', 'Aptenodytes')
+print(kesha.describe())
+print(kowalski.describe(True))
