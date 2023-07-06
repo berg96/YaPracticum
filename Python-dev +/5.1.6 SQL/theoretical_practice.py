2a4584ec727d02fd943fd577cf3242cc9eeb1d56
Artem Kulikov <chigar2010@yandex.ru>
2023-07-06 20:19:35 +0200

    use WHERE

--- Python-dev +/5.1.6 SQL/theoretical_practice.py
+++ Python-dev +/5.1.6 SQL/theoretical_practice.py
@@ -8,7 +8,8 @@ cur = con.cursor()
 results = cur.execute('''
   SELECT title,
          release_year
-  FROM video_products;
+  FROM video_products
+  WHERE release_year > 1980;
 ''')
 
 # В results получим итерируемый объект, который можно перебрать циклом:
