7d44f69f8dbc6ebed381d2b568fbd66b7c55ba81
Artem Kulikov <chigar2010@yandex.ru>
2023-07-10 14:45:52 +0200

    init

---
+++ Python-dev +/5.1 SQL/5.2.2.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+con = sqlite3.connect('db.sqlite')
+cur = con.cursor()
+
+cur.executescript(
+    '''
+CREATE TABLE IF NOT EXISTS original_titles(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS video_products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    original_title_id INTEGER NOT NULL UNIQUE,
+    FOREIGN KEY(original_title_id) REFERENCES original_titles(id)
+);
+'''
+)
+
+con.close()
